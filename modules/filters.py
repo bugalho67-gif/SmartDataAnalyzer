4fcdc965d04807358b8c22438854a5b79d3bb1c5
bugalho67-gif <bugalho67@gmail.com>
2026-07-26 12:27:35 -0300

    Add filter application function for DataFrame

---
+++ modules/filters.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+
+
+def apply_filters(df: pd.DataFrame):
+
+    st.sidebar.subheader("Filtros")
+
+    dataframe = df.copy()
+
+    for coluna in dataframe.columns:
+
+        if dataframe[coluna].dtype == "object":
+
+            valores = dataframe[coluna].dropna().unique()
+
+            if len(valores) <= 30:
+
+                selecionados = st.sidebar.multiselect(
+                    coluna,
+                    valores,
+                    default=valores
+                )
+
+                dataframe = dataframe[
+                    dataframe[coluna].isin(selecionados)
+                ]
+
+    return dataframe
