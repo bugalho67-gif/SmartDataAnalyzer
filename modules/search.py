6eaa1fba901cfbed2e135e0ab5ae7c284b5ac40c
bugalho67-gif <bugalho67@gmail.com>
2026-07-26 13:00:06 -0300

    Implement search functionality for DataFrame

---
+++ modules/search.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+def search_dataframe(df):
+
+    texto = st.sidebar.text_input(
+        "Pesquisar"
+    )
+
+    if texto:
+
+        mascara = df.astype(str).apply(
+            lambda coluna:
+            coluna.str.contains(
+                texto,
+                case=False,
+                na=False
+            )
+        )
+
+        df = df[
+            mascara.any(axis=1)
+        ]
+
+    return df
